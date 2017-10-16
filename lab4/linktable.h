6262ad28d19c9481bc578c1b2e145733e9854adb
ShuangYang516 <31958795+ShuangYang516@users.noreply.github.com>
2017-10-16 22:54:22 +0800

    Update linktable.h

--- lab4/linktable.h
+++ lab4/linktable.h
@@ -1,3 +1,32 @@
-#ifndef
+#ifndef _LINK_TABLE_H_
+#define _LINK_TABLE_H_
 
+#include <pthread.h>
+
+#define success 0
+#define failure (-1)
+
+typedef struct LinkTableNode
+{
+    struct LinkTableNode *pNext;
+}tLinkTableNode;
+typedef struct LinkTable
+{
+    tLinkTableNode *pHead;
+    tLinkTableNode *pTail;
+    int SumOfNode;
+    pthread_mutex_t mutex;
+}tLinkTable;
+tLinkTable * CreateLinkTable();
+//Delete linktable
+int DeleteLinkTable(tLinkTable * pLinkTable);
+//Add a linktable node
+void AddLinkTableNode(tLinkTable * pLinkTable, tLinkTableNode * pNode);
+//Delete a linktable node
+int DeleteLinkTableNode(tLinkTable * pLinkTable, tLinkTableNode * pNode);
+//Get linktable head
+tLinkTableNode * GetLinkTableHead(tLinkTable * pLinkTable);
+//Get next linktable node
+tLinkTableNode * GetNextLinkTableNode(tLinkTable * pLinkTable, tLinkTableNode * pNode);
+#endif
 
